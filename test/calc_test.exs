2d7e2426daa0d5e420b4775e5c76c4c66a249064
Piotr Grzesik <pj.grzesik@gmail.com>
2017-01-05 14:38:48 +0100

    Add first simple failing test

--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,7 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "adding 1 to 1 results in 2" do
+    assert Calc.add(1, 1) == 2
   end
 end
